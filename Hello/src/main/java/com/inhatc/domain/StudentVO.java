5d281e2f57f3a6d661c1b74ede2d3960bd688459
BaekTaeMin <sween45@naver.com>
2023-04-18 17:58:12 +0900

    tbl_studentinfo VO

---
+++ Hello/src/main/java/com/inhatc/domain/StudentVO.java
@@ -0,0 +1,29 @@
+package com.inhatc.domain;
+
+public class StudentVO {
+
+		private String No;
+		private String Name;
+		private int Code;
+		private String Addr;
+		private String Phone;
+		private String Email;
+		
+		public String getNo() { return No; }
+		public void setNo(String no) { No = no;	}
+		
+		public String getName() { return Name; }
+		public void setName(String name) { Name = name;	}
+		
+		public int getCode() { return Code;	}
+		public void setCode(int code) {	Code = code; }
+		
+		public String getAddr() { return Addr; }
+		public void setAddr(String addr) { Addr = addr; }
+		
+		public String getPhone() { return Phone; }
+		public void setPhone(String phone) { Phone = phone; }
+		
+		public String getEmail() { return Email; }
+		public void setEmail(String email) { Email = email; }
+}
