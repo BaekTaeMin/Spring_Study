657e44ef05e8a91582cf02417d73520ee01b127c
BaekTaeMin <sween45@naver.com>
2023-04-18 18:04:43 +0900

    Update Test

---
+++ Hello/src/test/java/com/inhatc/cs/StudentUpdateTest.java
@@ -0,0 +1,33 @@
+package com.inhatc.cs;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.inhatc.domain.StudentVO;
+import com.inhatc.persistence.StudentInfoDAO;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class StudentUpdateTest {
+
+		@Inject
+		private StudentInfoDAO dao;
+		
+		@Test
+		public void update() throws Exception{
+			StudentVO vo = new StudentVO();
+			vo.setNo("201944009");
+			vo.setName("±èÅÂ¹Î");
+			vo.setCode(44);
+			vo.setAddr("¼­¿ï ±ÝÃµ±¸");
+			vo.setPhone("01012345678");
+			vo.setEmail("sween125@gmail.com");
+			dao.update(vo);
+		}
+}
