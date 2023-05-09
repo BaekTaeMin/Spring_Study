4b4aeb142c4f2328c279007a08faba761019fd68
BaekTaeMin <sween45@naver.com>
2023-05-09 17:15:35 +0900

    RequestMethod

---
+++ Hello/src/main/java/com/inhatc/cs/BoardController.java
@@ -0,0 +1,35 @@
+package com.inhatc.cs;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.inhatc.domain.BoardVO;
+import com.inhatc.service.BoardService;
+
+@Controller
+@RequestMapping("/board/*") //localhost:port/board/*~~~~
+public class BoardController {
+
+	@Inject
+	private BoardService service;
+	
+	@RequestMapping(value="/register", method = RequestMethod.GET) //localhost:port/board/register
+	public String registerGET(BoardVO board, Model model) throws Exception {
+		System.out.println("Register (GET) Called");
+		return "/board/register2"; // /board/register.jsp
+	}
+	
+	@RequestMapping(value="/register", method = RequestMethod.POST) //localhost:port/board/register
+	public String registerPOST(BoardVO board, Model model) throws Exception {
+		System.out.println("Register (POST) Called");
+		System.out.println("제목 : " + board.getTitle());
+		System.out.println("내용 : " + board.getContent());
+		System.out.println("글쓴이 : " + board.getWriter());
+		service.regist(board);
+		return "home"; // /board/register.jsp
+	}
+}
